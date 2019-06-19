dbfdc19ef0670e18f6dd351382bb303c12fe67cf
Robin Paitry <paitrydev@gmail.com>
2019-06-19 11:20:57 +0200

    Add files via upload

---
+++ allillustration.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="description de la page">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" href="css/styleallillustration.css">
+    <link rel="stylesheet" href="css/styleA.css">
+
+    <!-- Custom CSS -->
+    <style>
+
+    </style>
+
+</head>
+<body>
+<h2 class="soustitre"> Mes Illustations </h2>
+
+<div class="container ">
+
+<!-- arbre -->
+
+<div class="row mb-3">
+        <div class="col-md-3 d-flex justify-content-center">
+            <a href="img/illarbre.jpg" target="-blank"><img src="img/arbre.jpg" class="radius"><p class="p1">Arbre<br>Gouache</p></a>
+
+
+        </div>
+        <!-- hangar -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illhangar.jpg" target="-blank"><img src="img/hangar.jpg" class="radius"><p class="p1">Hangar<br> Crayon à papier,<br> Photoshop</p></a>
+
+</div>
+
+<!-- temple inca -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illinca.jpg" target="-blank"><img src="img/inca.jpg" class="radius"><p class="p1">Temple Inca <br>Crayon à papier, <br>Photoshop</p></a>
+</div>
+
+
+<!-- esclave -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illesclave.jpg" target="-blank"><img src="img/esclave.jpg" class="radius"><p class="p1">L'esclave de <br>Michel-Ange<br>Fusain</p></a>
+</div>
+</div>
+
+
+
+
+
+
+
+<!-- 2ème ligne -->
+
+<!-- alligator-->
+<div class="row mb-3">
+        <div class="col-md-3 d-flex justify-content-center">
+            <a href="img/illcroco.jpg" target="-blank"><img src="img/alligator.jpg" class="radius"><p class="p1">Alligator<br> Crayon à papier,<br> Photoshop</p></a>
+
+
+        </div>
+        <!-- bd -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illBD.jpg" target="-blank"><img src="img/bd.jpg" class="radius" ><p class="p1">Bande-Dessinée <br>Crayon à papier,<br>Photoshop</p></a>
+
+</div>
+
+<!-- Kurt Cobain -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illkurt.jpg" target="-blank"><img src="img/kurt.jpg" class="radius"><p class="p1">Portrait de <br>Kurt Cobain <br>Mine de plomb</p></a>
+</div>
+
+
+<!-- legionnaire romain -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illromain.jpg" target="-blank"><img src="img/legionnaire.jpg" class="radius"><p class="p1">Model Sheet <br>Legionnaire Romain <br> Crayon à papier,<br>Photoshop</p></a>
+</div>
+</div>
+
+
+
+
+
+<!-- 3ème ligne -->
+
+<!-- fresque murale -->
+<div class="row mb-3">
+        <div class="col-md-3 d-flex justify-content-center">
+            <a href="https://www.behance.net/gallery/67040273/Fresque-Murale-Robin-Paitry?tracking_source=search%257Crobin%2Bpaitry" target="-blank"><img src="img/fresque.jpg" class="radius"><p class="p1">Fresque murale<br>11 mètres,<br>Peinture</p></a>
+
+
+        </div>
+        <!--buste féminin fusain -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illbuste.jpg" target="-blank"><img src="img/buste.jpg" class="radius" ><p class="p1">Buste féminin <br>Fusain</p></a>
+
+</div>
+
+
+
+
+        <!--mdel sheet -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illsheet.jpg" target="-blank"><img src="img/modelsheet.jpg" class="radius" ><p class="p1">Model Sheet <br>Crayon à papier</p></a>
+
+</div>
+
+
+
+        <!--dragon -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illdragon.jpg" target="-blank"><img src="img/dragon.jpg" class="radius" ><p class="p1">Model Sheet <br>d'un Dragon <br>Techniques diverses</p></a>
+
+</div>
+
+</div>
+
+
+<!-- 4ème ligne -->
+
+<!-- clé à molette -->
+<div class="row mb-3">
+        <div class="col-md-3 d-flex justify-content-center">
+            <a href="img/illclef.jpg" target="-blank"><img src="img/clef.jpg" class="radius"><p class="p1">Clé à molette<br>Gouache</p></a>
+
+
+        </div>
+        <!--gant de ski -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/illgant.jpg" target="-blank"><img src="img/gant.jpg" class="radius" ><p class="p1">Gant de ski<br>Gouache</p></a>
+
+</div>
+
+
+
+
+        <!--DA dragon -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/paitry03.avi" target="-blank"><img src="img/DAdragon.jpg" class="radius" ><p class="p1">Mini dessin-animé<br>" Ondulation "<br>encre de chine,<br>MonkeyJam</p></a>
+
+</div>
+
+
+
+        <!--dragon -->
+<div class="col-md-3 d-flex justify-content-center">
+<a href="img/paitry01.avi" target="-blank"><img src="img/DAmorphing.jpg" class="radius" ><p class="p1">Mini dessin-animé<br>" Le morphing "<br>encre de chine,<br>MonkeyJam</p></a>
+
+
+</div></div>
+
+<!-- bloc behance -->
+
+<div class="row mt-5 justify-content-center">
+<a href="https://www.behance.net/search?content=projects&sort=appreciations&time=week&featured_on_behance=true&search=robin%20paitry" target="-blank" class="p2 lienbehance">
+<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="100" height="100"
+viewBox="0 0 192 192"
+style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,192v-192h192v192z" fill="none"></path><g fill="#fef2e4"><path d="M34.56,15.36c-10.5984,0 -19.2,8.6016 -19.2,19.2v122.88c0,10.5984 8.6016,19.2 19.2,19.2h122.88c10.5984,0 19.2,-8.6016 19.2,-19.2v-122.88c0,-10.5984 -8.6016,-19.2 -19.2,-19.2zM46.08,69.12h25.845c2.8032,0 17.97,-0.1908 17.97,13.71c0,7.3728 -5.0319,9.8664 -7.2975,10.98c3.3792,1.0752 9.5625,4.1868 9.5625,13.71c0,14.7072 -16.8594,15.36 -19.125,15.36h-26.955zM111.36,69.12h26.88v7.68h-26.88zM57.6,78.45v12.6h12.3675c1.6896,0 7.29,-0.9252 7.29,-6.57c0,-5.6448 -7.2939,-6.03 -8.4075,-6.03zM125.685,80.64c14.976,0 19.0431,11.6727 19.6575,14.4375c0.576,2.8032 0.5775,5.2617 0.5775,8.6025h-30.3375c0,3.3408 1.7703,10.8675 10.7175,10.8675c2.3808,0 4.1427,-0.5739 5.9475,-1.6875c1.7664,-1.1136 2.3865,-2.1939 2.9625,-3.3075h10.095c-1.7664,4.416 -4.1463,7.7628 -7.7175,9.99c-3.5712,2.2272 -7.7184,3.3375 -12.48,3.3375c-2.9952,0 -5.9523,-0.5739 -8.9475,-1.6875c-2.3808,-1.1136 -4.7586,-2.7663 -6.525,-4.4175c-1.7664,-1.6896 -2.9571,-3.9177 -4.1475,-6.6825c-1.1904,-2.2272 -1.8075,-5.5677 -1.8075,-8.3325c0,-2.7648 1.1154,-21.12 22.005,-21.12zM125.685,88.4325c-8.6784,0 -10.1025,8.3349 -10.1025,8.8725h19.05c-0.6144,-2.7648 -3.1107,-8.8725 -8.9475,-8.8725zM57.6,98.73v14.82h12.87c1.10976,0 8.94,-0.3432 8.94,-7.14c0,-6.7584 -6.15216,-7.68 -8.94,-7.68z"></path></g></g></svg>
+</div><p class="p2"> Pour plus de projets, retrouvez moi sur Behance</p></a>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- CONTENT START -->
+    <?php
+    include ('navbar.php')
+    ?>
+    <!-- CONTENT END -->
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="js/main.js"></script>
+</body>
+</html>
