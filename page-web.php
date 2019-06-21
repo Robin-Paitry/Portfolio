bcdaeaca0d8341265e7d99925e508d6dd7d040aa
Robin Paitry <paitrydev@gmail.com>
2019-06-21 13:15:22 +0200

    Add files via upload

---
+++ page-web.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="description de la page">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" href="css/styleweb.css">
+    <link rel="stylesheet" href="css/styleA.css">
+
+    <!-- Custom CSS -->
+    <style>
+
+
+    </style>
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/code-new-roman" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- CONTENT START -->
+    <?php
+   include ('navbar.php')
+   ?>
+
+<h2 class="soustitre"> MES PROJETS WEB </h2>
+
+<div class="container ">
+
+<!-- pendu -->
+
+    <div class="row mb-3">
+        <div class="col-md-4 d-flex justify-content-center">
+            <a href="pendu/pendu3.html" target="-blank"><img src="img/pendu2.jpg"><p class="p1 textpendu ">Jeu du Pendu<br>( HTML et JavaScript )</p></a>
+
+
+        </div>
+        <!-- calculatrice -->
+<div class="col-md-4 d-flex justify-content-center">
+<a href="calculatrice/calculatrice-finale.html" target="-blank"><img src="img/calculatrice2.jpg" ><p class="p1 textcalculatrice">Calculatrice<br>( HTML et JavaScript )</p></a>
+
+</div>
+
+<!-- morpion -->
+<div class="col-md-4 d-flex justify-content-center">
+<a href="morpion/Griche_Myriem_Martin_Caroline_Paitry_Robin_Morpion.html" target="-blank"><img src="img/morpion2.jpg"><p class="p1 textmorpion">Jeu du Morpion <br>( HTML et JavaScript )</p></a>
+</div>
+</div>
+
+<!-- blueasy -->
+<div class="row">
+    <div class="col-md-4 d-flex justify-content-center">
+            <a href="blueSite/index.html" target="-blank"><img src="img/blue2.jpg"><p class="p1">Site BluEasy<br>( HTML et JavaScript )<p></a>
+</div>
+
+<!-- starwars -->
+<div class="col-md-4  d-flex justify-content-center">
+<a href="star-wars2/index.php"  target="-blank"><img src="img/starwars2.jpg"><p class="p1">Site Star warriors<br>( HTML et PHP)<p></a>         
+</div>
+
+<!-- liste bd -->
+<div class="col-md-4 d-flex justify-content-center">
+            <a href="liste.php" target="-blank"><img src="img/liste.jpg" class=""><p class="p1">Base de donnée modifiable<br>
+            Partie admin et utilisateur<br>( symfony, SQL et PHP )<p></a>
+            
+</div>
+</div>
+<div class="container">
+
+<!-- tous les projets -->
+
+    <!-- <div class="row">
+        <div class="col-12 d-flex justify-content-center mt-5">
+            <a href="" ><img src="img/touslesprojets.png"></a>
+
+
+        </div></div></div> -->
+
+
+
+
+   
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
