0416fd3c5b56577f943fa7236b624f00f97bbb1d
Robin Paitry <paitrydev@gmail.com>
2019-06-21 13:15:01 +0200

    Add files via upload

---
+++ navbar-2.php
@@ -0,0 +1,61 @@
+<header>
+<div class="container-fluid">
+<div class="row">
+<div class="col-12">
+<div class="pos-f-t">
+<div class="collapse" id="navbarToggleExternalContent">
+<div>
+
+</div>
+</div>
+<nav class="navbar navbar-dark ">
+<div class="site-menu" id="site-menu">
+<div class="site-menu-background js-close-menu"></div>
+<button class="site-menu-sidebar js-open-menu">
+<span class="line line-1"></span>
+<span class="line line-2"></span>
+<span class="line line-3"></span>
+</button>
+
+<div class="site-menu-inner">
+<div class="site-menu-container">
+<!-- Menu Header -->
+<div class="site-menu-header">
+<span class="site-menu-suptitle"><span class="text">Robin PAITRY</span></span>
+<button type="button" class="site-menu-close js-close-menu">
+<span class="u-visually-hidden">Close menu</span>
+<span class="line line-1"></span>
+<span class="line line-2"></span>
+</button>
+</div>
+<!-- Menu navigation -->
+<nav class="site-navigation">
+<ul class="site-navigation-list" style="list-style: none;">
+<li class="site-navigation-item">
+<a class="site-navigation-link" href="index-2.php">
+<span class="link-text">Accueil</span>
+</a>
+</li>
+<li class="site-navigation-item">
+<a class="site-navigation-link" href="apropos-3.php">
+<span class="link-text">À propos</span>
+</a>
+</li>
+
+<li class="site-navigation-item">
+<a class="site-navigation-link" href="page-web-2.php">
+<span class="link-text vw-100">Développement Web</span>
+</a>
+</li>
+
+<li class="site-navigation-item">
+<a class="site-navigation-link " href="contact.php">
+<span class="link-text">Contact</span>
+</a>
+</li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</nav>
